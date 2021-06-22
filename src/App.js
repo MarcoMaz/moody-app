0661be611895ee0296d57b2b6fc901b557c33b62
Marco Mazzai <mazzai.marco@gmail.com>
2021-06-22 23:12:42 +0200

    change router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 
 import Intro from './views/Intro'
 import Question1 from './views/Question1'
