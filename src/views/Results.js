329c92d4d4c107c15a885439a7276771d7154409
Marco Mazzai <mazzai.marco@gmail.com>
2021-06-23 19:33:00 +0200

    add logic

--- src/views/Results.js
+++ src/views/Results.js
@@ -1,6 +1,13 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom'
+
 import audio from '../dormono.mp3';
 
+import fakeData from './fakeData'
+
 const Results = () => {
+	const [ activeAlbum, setActiveAlbum ] = useState('')
+	const [ visibleAlbum, setVisibleAlbum ] = useState(false)
 
 	let newAudio = new Audio(audio)
 
@@ -12,15 +19,33 @@ const Results = () => {
 		newAudio.pause()
 	}
 
+	const handleClick = (music) => {
+		setActiveAlbum(music.album)
+		setVisibleAlbum(true)
+	}
 
 	return(
 		<div>
-			questi sono i risultati
-			<div>
-				<strong>Io sono un risultato</strong><br/>
-				<button onClick={start}>Play me!</button>
-				<button onClick={stop}>Stop me!</button>
-			</div>
+			<p>questi sono i risultati</p>
+			<ul>
+				{
+					fakeData.rosi.map((music, index) => (
+						<li onClick={() => handleClick(music)} key={index}>{music.album}</li>
+					))
+				}
+			</ul>
+			{
+				visibleAlbum && 
+				<div>
+					<strong>Hai scelto { activeAlbum}</strong><br/>
+					<button onClick={start}>Play me!</button>
+					<button onClick={stop}>Stop me!</button>
+					<button onClick={() => setVisibleAlbum(false)}>Cambia canzone</button>
+				</div>
+			}
+			<Link to="/">
+				<button>Ritorna all'inizio</button>
+			</Link>
 		</div>
 	)
 }
---
+++ src/views/fakeData.js
@@ -0,0 +1,21 @@
+const fakeData = {
+	"rosi": [
+		{
+			"album": "Plini"
+		},
+		{
+			"album": "Dream Theater"
+		},
+		{
+			"album": "The dark side of the moon"
+		},
+		{
+			"album": "Michael Buble"
+		},
+		{
+			"album": "The clash"
+		}
+	]
+}
+
+export default fakeData
