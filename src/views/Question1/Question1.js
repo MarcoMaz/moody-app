39ef6ee63507bf56b979ee674af2f8b09a842dd3
Marco Mazzai <mazzai.marco@gmail.com>
2021-07-03 12:17:11 +0200

    add disabled button logic

--- src/views/Question1/Question1.js
+++ src/views/Question1/Question1.js
@@ -1,13 +1,24 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import './Question.scss'
 
 const Question1 = () => {
+	const [ question1, setQuestion1 ] = useState('');
+
+	function SubmitButton(){
+    if (question1){
+      return <button type="button">Go!</button>
+    } else {
+      return <button type="button" disabled>Disabled</button>
+    };
+  };
+
 	return(
 		<section className="Question Question-1">
 			<div className="Question__label">
 				<label htmlFor="question1">Question #1</label>
-				<input type="text" id="question1" name="question1" required placeholder="Question #1"/>
+				<input value={question1} onChange={ e => setQuestion1(e.target.value)} type="text" id="question1" name="question1" required placeholder="Question #1"/>
 			</div>
 			<figure className="Question__image">
 				<img alt="This is a text" src="https://via.placeholder.com/150"></img>
@@ -24,8 +35,9 @@ const Question1 = () => {
 				<div className="Question__arrows__left"></div>
 				<div className="Question__arrows__right"></div>
 			</div>
+
 			<Link to="/elaborating">
-				<button>Go!</button>
+      	<SubmitButton/>
 			</Link>		
 		</section>
 	)
