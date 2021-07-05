4e409c3fb2070e6a8472ce522da5c2fb2a5b0d33
Marco Mazzai <mazzai.marco@gmail.com>
2021-07-05 15:32:19 +0200

    fix results styling

--- src/views/fakeData.js
+++ src/views/fakeData.js
@@ -3,27 +3,27 @@ const fakeData = {
 		{
 			"title": "Scented",
 			"artist": "Plini",
-			"imageUrl": "https://via.placeholder.com/150"
+			"imageUrl": "https://via.placeholder.com/120"
 		},
 		{
 			"title": "The Mirror",
 			"artist": "Dream Theater",
-			"imageUrl": "https://via.placeholder.com/150"
+			"imageUrl": "https://via.placeholder.com/120"
 		},
 		{
 			"title": "Wish you were here",
 			"artist": "Pink Floyd",
-			"imageUrl": "https://via.placeholder.com/150"
+			"imageUrl": "https://via.placeholder.com/120"
 		},
 		{
 			"title": "I love doing it",
 			"artist": "Michael Buble",
-			"imageUrl": "https://via.placeholder.com/150"
+			"imageUrl": "https://via.placeholder.com/120"
 		},
 		{
 			"title": "London Calling",
 			"artist": "The Clash",
-			"imageUrl": "https://via.placeholder.com/150"
+			"imageUrl": "https://via.placeholder.com/120"
 		}
 	]
 }
