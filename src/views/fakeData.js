75cfcad32932dc5aab257387c5fa5fdb21c7a35b
Marco Mazzai <mazzai.marco@gmail.com>
2021-07-07 22:57:32 +0200

    add images

--- src/views/fakeData.js
+++ src/views/fakeData.js
@@ -1,3 +1,4 @@
+// Import Songs
 import africa from '../songs/africa.mp3';
 import deandre from '../songs/cover-deandre.mp3';
 import ninasimone from '../songs/cover-ninasimone.mp3';
@@ -11,6 +12,20 @@ import verdena1 from '../songs/cover-verdena1.mp3';
 import verdena2 from '../songs/cover-verdena2.mp3';
 import whitestripes from '../songs/cover-whitestripes.mp3';
 import wolfmother from '../songs/cover-wolfmother.mp3';
+
+// Import Cover Arts
+import deandreImg from '../images/cover-deandre.jpg';
+import ninasimoneImg from '../images/cover-ninasimone.jpg';
+import rinogaetanoImg from '../images/cover-rinogaetano.jpg';
+import sexpistolsImg from '../images/cover-sexpistols.jpg';
+import tenaciousdImg from '../images/cover-tenaciousd.jpg';
+import thebeatlesImg from '../images/cover-thebeatles.jpg';
+import themcrookedvulturesImg from '../images/cover-themcrookedvultures.jpg';
+import themetersImg from '../images/cover-themeters.jpg';
+import verdena1Img from '../images/cover-verdena1.jpg';
+import verdena2Img from '../images/cover-verdena2.jpg';
+import whitestripesImg from '../images/cover-whitestripes.jpg';
+import wolfmotherImg from '../images/cover-wolfmother.jpg';
 
 const fakeData = {
 	"rosi": [
@@ -55,73 +70,73 @@ const fakeData = {
 		{
 			"title": "Dormono sulla collina",
 			"artist": "Fabrizio DeAndre'",
-			"imageUrl": "https://via.placeholder.com/120",
+			"imageUrl": deandreImg,
 			"song": deandre
 		},
 		{
 			"title": "Feeling Good",
 			"artist": "Nina Simone",
-			"imageUrl": "https://via.placeholder.com/120",
+			"imageUrl": ninasimoneImg,
 			"song": ninasimone
 		},
 		{
 			"title": "Berta Filava",
 			"artist": "Rino Gaetano",
-			"imageUrl": "https://via.placeholder.com/120",
+			"imageUrl": rinogaetanoImg,
 			"song": rinogaetano
 		},
 		{
 			"title": "No Feelings",
 			"artist": "Sex Pistols",
-			"imageUrl": "https://via.placeholder.com/120",
+			"imageUrl": sexpistolsImg,
 			"song": sexpistols
 		},
 		{
 			"title": "Classico",
 			"artist": "Tenacious D",
-			"imageUrl": "https://via.placeholder.com/120",
+			"imageUrl": tenaciousdImg,
 			"song": tenaciousd
 		},
 		{
 			"title": "Sexy Sadie",
 			"artist": "The Beatles",
-			"imageUrl": "https://via.placeholder.com/120",
+			"imageUrl": thebeatlesImg,
 			"song": thebeatles
 		},
 		{
 			"title": "Cissy Strut",
 			"artist": "The Meters",
-			"imageUrl": "https://via.placeholder.com/120",
+			"imageUrl": themetersImg,
 			"song": themeters
 		},
 		{
 			"title": "Elephant",
 			"artist": "Them Crooked Vultures",
-			"imageUrl": "https://via.placeholder.com/120",
+			"imageUrl": themcrookedvulturesImg,
 			"song": themcrookedvultures
 		},
 		{
 			"title": "Scegli me",
 			"artist": "Verdena",
-			"imageUrl": "https://via.placeholder.com/120",
+			"imageUrl": verdena1Img,
 			"song": verdena1
 		},
 		{
 			"title": "Sipario",
 			"artist": "Verdena",
-			"imageUrl": "https://via.placeholder.com/120",
+			"imageUrl": verdena2Img,
 			"song": verdena2
 		},
 		{
 			"title": "Catch Hell Blues",
 			"artist": "The White Stripes",
-			"imageUrl": "https://via.placeholder.com/120",
+			"imageUrl": whitestripesImg,
 			"song": whitestripes
 		},
 		{
 			"title": "Woman",
 			"artist": "Wolfmother",
-			"imageUrl": "https://via.placeholder.com/120",
+			"imageUrl": wolfmotherImg,
 			"song": wolfmother
 		}
 	]
