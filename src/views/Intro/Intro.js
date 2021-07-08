de01d45f9914068a3ab3c18bd8965a0e6a4ab317
Marco Mazzai <mazzai.marco@gmail.com>
2021-07-08 16:35:58 +0200

    hide activate filter button

--- src/views/Intro/Intro.js
+++ src/views/Intro/Intro.js
@@ -7,19 +7,25 @@ import './Intro.scss'
 const Intro = () => {
 	const { setIsFilterActive } = useContext(Rosify)
 
+	let counter = 0;
+
+	const activateFilter = () => {
+		counter++
+		if (counter === 2){
+			setIsFilterActive(true)
+		}
+	}
+
 	return(
 		<section className="Intro">
 			<h1 className="Intro__headline">This is the headline</h1>
-			<figure className="Intro__image">
+			<figure className="Intro__image" onClick={activateFilter}>
 				<img alt="This is a text" src="https://via.placeholder.com/150"></img>
 			</figure>
 			<h2 className="Intro__subheadline">This is the subheadline</h2>
 			<p className="Intro__paragraph">This is a very long paragraphThis is a very long paragraphThis is a very long paragraphThis is a very long paragraphThis is a very long paragraphThis is a very long paragraphThis is a very long paragraph</p>
 			<Link to="/question1">
-				<button className="Intro__button">Start!!</button><br/><br/>
-				<button onClick={() => setIsFilterActive(true)}>
-					Attiva il filtro
-				</button>
+				<button className="Intro__button">Start!!</button>
 			</Link>
 		</section>
 	)
