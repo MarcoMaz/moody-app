49cf546ca5052aeef10132bfa8c6e06a022984fe
Marco Mazzai <mazzai.marco@gmail.com>
2021-07-13 22:45:16 +0200

    add checkboxes

--- src/views/Questions/Question2.js
+++ src/views/Questions/Question2.js
@@ -8,10 +8,11 @@ import question2Image from '../../images/image-question-02.png';
 import './Question.scss'
 
 const Question2 = () => {
-	const [ something, setSomething ] = useState('')
+  const [checked, setChecked] = useState(false)
+  const handleClick = () => setChecked(true)
 
 	function SubmitButton(){
-    if (something){
+    if (checked){
       return <button className="Question__button" type="button">Go!</button>
     } else {
       return <button type="button" className="Question__button -disabled" disabled>Go!</button>
@@ -27,16 +28,19 @@ const Question2 = () => {
 				<span className="Question__progress__numbers">2 di 3</span>
 			</div>
 			<div className="Question__label">
-				<label htmlFor="question2">Question2</label>
-				<input 
-					value={something} 
-					type="text" 
-					id="question2" 
-					name="question2"  
-					placeholder="Inserisci il tuo nome"
-					onChange={ e => setSomething(e.target.value)} 
-					required
-					/>
+				<p>Per cosa ti serve?</p>
+				<div className="Question__choice1">
+					<input type="radio" id="questionChoice1" name="contact" value="email" onChange={handleClick} />
+    			<label htmlFor="questionChoice1">Email</label>
+				</div>
+				<div className="Question__choice2">
+					<input type="radio" id="questionChoice2" name="contact" value="phone" onChange={handleClick} />
+    			<label htmlFor="questionChoice2">Phone</label>
+				</div>
+				<div className="Question__choice3">
+					<input type="radio" id="questionChoice3" name="contact" value="fax" onChange={handleClick} />
+    			<label htmlFor="questionChoice3">Fax</label>
+				</div>
 			</div>
 			<figure className="Question__image">
 				<img alt="This is a text" src={question2Image}></img>
--- src/views/Questions/Question3.js
+++ src/views/Questions/Question3.js
@@ -8,10 +8,11 @@ import question3Image from '../../images/image-question-03.png';
 import './Question.scss'
 
 const Question3 = () => {
-	const [ something, setSomething ] = useState('')
+  const [checked, setChecked] = useState(false)
+  const handleClick = () => setChecked(true)
 
 	function SubmitButton(){
-    if (something){
+    if (checked){
       return <button className="Question__button" type="button">Go!</button>
     } else {
       return <button type="button" className="Question__button -disabled" disabled>Go!</button>
@@ -21,22 +22,35 @@ const Question3 = () => {
 	return(
 		<section className="Question Question-3">
 			<div className="Question__progress">
-				<Link to="/Question2">
+				<Link to="/question2">
 					<button className="Question__progress__back"><FontAwesomeIcon icon="chevron-left" /></button>
 				</Link>
 				<span className="Question__progress__numbers">3 di 3</span>
 			</div>
 			<div className="Question__label">
-				<label htmlFor="question3">Question 3</label>
-				<input 
-					value={something} 
-					type="text" 
-					id="question3" 
-					name="question3"  
-					placeholder="Inserisci il tuo nome"
-					onChange={ e => setSomething(e.target.value)} 
-					required
-					/>
+				<p>Quanto sei felice oggi? <br/>
+					0 poco, 5 tanto
+				</p>
+				<div className="Question__choice1">
+					<input type="radio" id="questionChoice1" name="contact" value="1" onChange={handleClick} />
+    			<label htmlFor="questionChoice1">1</label>
+				</div>
+				<div className="Question__choice2">
+					<input type="radio" id="questionChoice2" name="contact" value="2" onChange={handleClick} />
+    			<label htmlFor="questionChoice2">2</label>
+				</div>
+				<div className="Question__choice3">
+					<input type="radio" id="questionChoice3" name="contact" value="3" onChange={handleClick} />
+    			<label htmlFor="questionChoice3">3</label>
+				</div>
+				<div className="Question__choice4">
+					<input type="radio" id="questionChoice4" name="contact" value="4" onChange={handleClick} />
+    			<label htmlFor="questionChoice4">4</label>
+				</div>
+				<div className="Question__choice5">
+					<input type="radio" id="questionChoice5" name="contact" value="5" onChange={handleClick} />
+    			<label htmlFor="questionChoice5">5</label>
+				</div>
 			</div>
 			<figure className="Question__image">
 				<img alt="This is a text" src={question3Image}></img>
