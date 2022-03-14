d545fae98a245afde0cc22ed482c3b09aba7ea70
Marco Mazzai <mazzai.marco@gmail.com>
2022-03-14 21:11:12 +0100

    fix intro

--- src/views/Intro/Intro.js
+++ src/views/Intro/Intro.js
@@ -1,36 +1,35 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-import background from "../../assets/moody-logo.png";
-import copyText from "../../assets/copyText";
-import introImage from "../../assets/image-intro.png";
+import background from '../../assets/moody-logo.png';
+import copyText from '../../assets/copyText';
+import introImage from '../../assets/image-intro.png';
 
-import "./Intro.scss";
+import './Intro.scss';
 
 const Intro = () => {
-  const { headline, appName, subHeadline, paragraphs, cta } = copyText.intro;
+	const { headline, appName, subHeadline, paragraphs, cta } = copyText.intro;
+	const appNameWithImage = appName.slice(1);
 
-  const appNameWithImage = appName.slice(1);
+	return (
+		<section className="Intro">
+			<h1 className="Intro__headline">
+				{headline}
+				<span style={{ backgroundImage: `url(${background})` }}>
+					{appNameWithImage}
+				</span>
+			</h1>
+			<figure className="Intro__image">
+				<img alt="intro" src={introImage}></img>
+			</figure>
+			<h2 className="Intro__subheadline">{subHeadline}</h2>
+			{paragraphs.map((paragraph, index) => (
+				<p key={index} className="Intro__paragraph">{paragraph}</p>
+			))}
+			<Link to="/question1">
+				<button className="Intro__button">{cta}</button>
+			</Link>
+		</section>
+	)
+}
 
-  return (
-    <section className="Intro">
-      <h1 className="Intro__headline">
-        {headline}
-        <span style={{ backgroundImage: `url(${background})` }}>
-          {appNameWithImage}
-        </span>
-      </h1>
-      <figure className="Intro__image">
-        <img alt="intro app" src={introImage}></img>
-      </figure>
-      <h2 className="Intro__subheadline">{subHeadline}</h2>
-      {paragraphs.map((paragraph) => (
-        <p className="Intro__paragraph">{paragraph}</p>
-      ))}
-      <Link to="/question1">
-        <button className="Intro__button">{cta}</button>
-      </Link>
-    </section>
-  );
-};
-
-export default Intro;
+export default Intro
