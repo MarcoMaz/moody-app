bfd582472ad8982ecd34c48281f68eb94d8aa880
Marco Mazzai <mazzai.marco@gmail.com>
2022-03-16 21:48:36 +0100

    fix body error

--- src/views/Results/Results.tsx
+++ src/views/Results/Results.tsx
@@ -1,28 +1,28 @@
-import { useState, useRef, useEffect, useContext, useMemo } from "react";
-import { Moodify } from "../../App";
-import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState, useRef, useEffect, useContext, useMemo } from 'react'
+import { Moodify } from '../../App'
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-import fakeSong from "../../assets/africa.mp3";
-import copyText from "../../assets/copyText";
+import fakeSong from '../../assets/africa.mp3'
+import copyText from '../../assets/copyText'
 
-import Modal from "./Modal";
-import fakeData from "../fakeData";
+import Modal from './Modal'
+import fakeData from '../fakeData'
 
-import "./Results.scss";
+import './Results.scss'
 
 const Results = () => {
-  const { setUsername } = useContext(Moodify);
+  const { setUsername } = useContext(Moodify)
 
   const [activeChoice, setActiveChoice] = useState({
-    imageUrlChoice: "",
-    titleChoice: "",
-    artistChoice: "",
-    songChoice: "",
-  });
+    imageUrlChoice: '',
+    titleChoice: '',
+    artistChoice: '',
+    songChoice: '',
+  })
 
-  const [visibleAlbum, setVisibleAlbum] = useState(false);
-  const [isPlaying, setIsPlaying] = useState(false);
+  const [visibleAlbum, setVisibleAlbum] = useState(false)
+  const [isPlaying, setIsPlaying] = useState(false)
 
   const {
     headline,
@@ -31,48 +31,48 @@ const Results = () => {
     startOver,
     imageAlt,
     imageModalAlt,
-  } = copyText.results;
+  } = copyText.results
 
   const newPlaylist = useMemo(() => {
-    let chosePlaylist = fakeData;
+    let chosePlaylist = fakeData
 
     // Add a random key
-    let randomSortKey = {};
+    let randomSortKey = {}
 
-    chosePlaylist.forEach((d) => (randomSortKey[d] = Math.random()));
+    chosePlaylist.forEach((d) => (randomSortKey[d] = Math.random()))
 
     //add the sortKey property to the individual array entries
     let dataSortable = chosePlaylist.map((playlist) => {
       return {
         ...playlist,
         sortKey: Math.random(),
-      };
-    });
+      }
+    })
 
     // Sort the List, cut it to six values and create a new playlist
-    return dataSortable.sort((a, b) => a.sortKey - b.sortKey).slice(0, 6);
-  }, []);
+    return dataSortable.sort((a, b) => a.sortKey - b.sortKey).slice(0, 6)
+  }, [])
 
-  let audio = useRef();
+  let audio = useRef()
 
   const start = (selectedSong) => {
-    audio.current = new Audio(activeChoice.songChoice);
-    audio.current.play();
-    setIsPlaying(true);
-  };
+    audio.current = new Audio(activeChoice.songChoice)
+    audio.current.play()
+    setIsPlaying(true)
+  }
 
   const stop = () => {
     if (audio.current !== undefined) {
-      audio.current.pause();
+      audio.current.pause()
     }
-    setIsPlaying(false);
-  };
+    setIsPlaying(false)
+  }
 
   const clickChangeSong = () => {
-    setVisibleAlbum(false);
-    stop();
-    setIsPlaying(false);
-  };
+    setVisibleAlbum(false)
+    stop()
+    setIsPlaying(false)
+  }
 
   const handleClick = (music) => {
     setActiveChoice({
@@ -80,19 +80,23 @@ const Results = () => {
       titleChoice: music.title,
       artistChoice: music.artist,
       songChoice: fakeSong,
-    });
-    setVisibleAlbum(true);
-  };
+    })
+    setVisibleAlbum(true)
+  }
 
   const clickReset = () => {
-    setUsername("");
-  };
+    setUsername('')
+  }
 
   useEffect(() => {
-    visibleAlbum === true
-      ? document.querySelector("body").classList.add("blocked")
-      : document.querySelector("body").classList.remove("blocked");
-  });
+    const bodyElement = document.querySelector('body')
+
+    if (bodyElement) {
+      visibleAlbum === true
+        ? bodyElement.classList.add('blocked')
+        : bodyElement.classList.remove('blocked')
+    }
+  })
 
   return (
     <>
@@ -148,7 +152,7 @@ const Results = () => {
         }
       </section>
     </>
-  );
-};
+  )
+}
 
-export default Results;
+export default Results
