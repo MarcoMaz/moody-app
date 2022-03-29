543ac7ba6e0c0ebfc1e5d315fa7f6e06099eac92
Marco Mazzai <mazzai.marco@gmail.com>
2022-03-29 10:42:39 +0200

    feat( question3 ): - add tests - fix test in question 2

--- src/views/Questions/__test__/Question3.test.js
+++ src/views/Questions/__test__/Question3.test.js
@@ -2,91 +2,97 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
-import Question2 from '../Question2';
+import Question3 from '../Question3';
 
-const Question2Element = (
+const Question3Element = (
   <BrowserRouter>
-    <Question2 />
+    <Question3 />
   </BrowserRouter>
 );
 
-describe('The Question #2 view renders', () => {
+describe('The Question #3 view renders', () => {
   it('the chevron-left icon', () => {
-    render(Question2Element);
+    render(Question3Element);
     const chevronLeftElement = screen.getByLabelText('chevron-left-icon');
     expect(chevronLeftElement).toBeInTheDocument();
   });
 
   it('the progress', () => {
-    render(Question2Element);
-    const progressElement = screen.getByText('2 / 3');
+    render(Question3Element);
+    const progressElement = screen.getByText('3 / 3');
     expect(progressElement).toBeInTheDocument();
   });
 
   it('the radio button label', () => {
-    render(Question2Element);
-    const radioButtonLabelElement = screen.getByText(
-      'Usually, where do you listen to music ?',
-    );
+    render(Question3Element);
+    const radioButtonLabelElement = screen.getByText("How's your mood today ?");
     expect(radioButtonLabelElement).toBeInTheDocument();
   });
 
   it('the radio buttons', () => {
-    render(Question2Element);
+    render(Question3Element);
     const radioButtonsElement = screen.getAllByRole('radio');
-    expect(radioButtonsElement).toHaveLength(4);
+    expect(radioButtonsElement).toHaveLength(5);
   });
 
   it('the first radio button', () => {
-    render(Question2Element);
+    render(Question3Element);
     const radioButton1Element = screen.getByRole('radio', {
-      name: 'In my car',
+      name: 'Very bad',
     });
     expect(radioButton1Element).toBeInTheDocument();
   });
 
   it('the second radio button', () => {
-    render(Question2Element);
+    render(Question3Element);
     const radioButton2Element = screen.getByRole('radio', {
-      name: 'While I do jogging',
+      name: 'Bad',
     });
     expect(radioButton2Element).toBeInTheDocument();
   });
 
   it('the third radio button', () => {
-    render(Question2Element);
+    render(Question3Element);
     const radioButton3Element = screen.getByRole('radio', {
-      name: 'In my bathtub',
+      name: 'Normal',
     });
     expect(radioButton3Element).toBeInTheDocument();
   });
 
   it('the fourth radio button', () => {
-    render(Question2Element);
+    render(Question3Element);
     const radioButton4Element = screen.getByRole('radio', {
-      name: 'Somewhere else...',
+      name: 'Good',
     });
     expect(radioButton4Element).toBeInTheDocument();
   });
 
+  it('the fifth radio button', () => {
+    render(Question3Element);
+    const radioButton5Element = screen.getByRole('radio', {
+      name: 'Very good!',
+    });
+    expect(radioButton5Element).toBeInTheDocument();
+  });
+
   it('the button', () => {
-    render(Question2Element);
+    render(Question3Element);
     const buttonElement = screen.getByRole('button', {
-      name: 'Almost there...',
+      name: 'Go!',
     });
     expect(buttonElement).toBeInTheDocument();
   });
 
   it('the button disabled', () => {
-    render(Question2Element);
+    render(Question3Element);
     const buttonElement = screen.getByRole('button', {
-      name: 'Almost there...',
+      name: 'Go!',
     });
     expect(buttonElement).toBeDisabled();
   });
 
   it('the image', () => {
-    render(Question2Element);
+    render(Question3Element);
     const imageElement = screen.getByRole('figure');
     expect(imageElement).toBeInTheDocument();
   });
